7073b89fa131b084ed596bb0cfd940336b738794
Mikey786 <n.mohankrishna2002@gmail.com>
2024-09-17 15:07:09 +0530

    Resolved erros in Eswar

--- Eswar.java
+++ Eswar.java
@@ -1,27 +1,28 @@
-class Box{
-double len,bre,hei;
-Box( double l, double b, double h){
-    len = l;
-    bre=b;
-    hei=h;
-}
-Void display(){
-    System.out.println("length : " + len);
-    System.out.println("breadth : " + bre);
-    System.out.println("height : " + hei);
+class Box {
+    double len, bre, hei;
 
-}
-double cal_vol(){
-    return(len*bre*hei);
+    Box(double l, double b, double h) {
+        len = l;
+        bre = b;
+        hei = h;
+    }
+
+    void display() {
+        System.out.println("Length : " + len);
+        System.out.println("Breadth : " + bre);
+        System.out.println("Height : " + hei);
+    }
+
+    double cal_vol() {
+        return (len * bre * hei);
+    }
 }
 
-
-}
-class BoxTest(){
+public class Eswar {
     public static void main(String[] args) {
-        Box ob = new Box(2.4,5.6,7.4);
-        double v = ob.cal_vol();
-        System.out.println("Box object ob : " + ob.display());
-        System.out.println("Volume :" + v);
+        Box ob = new Box(2.4, 5.6, 7.4);
+        ob.display();  // Display the dimensions of the box
+        double v = ob.cal_vol();  // Calculate the volume
+        System.out.println("Volume: " + v);  // Print the volume
     }
 }
